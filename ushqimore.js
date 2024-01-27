b74205df33cf471c8dbb90a77b8ff36de8f0b5c8
edonberishaa <150199937+edonberishaa@users.noreply.github.com>
2024-01-27 22:51:12 +0100

    Update ushqimore.js

--- ushqimore.js
+++ ushqimore.js
@@ -1,1 +1,14 @@
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+window.onload = function() {
+    // Prompt for name
+    var enteredName = prompt("Please enter your name:");
+
+    // Check if a name was entered
+    if (enteredName !== null && enteredName !== "") {
+        // Display the entered name in an alert
+        // Append the name to the heading in the HTML
+        var heading = document.getElementById("name");
+        heading.innerHTML = "Pershendetje " + enteredName + "!" + " Shiko";
+    } else {
+        alert("No name entered.");
+    }
+};
